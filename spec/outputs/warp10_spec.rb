e1c8f2934521bf106778e4b31e0054847992db67
Alexandre DUVAL <alexandre.duval@insa-rouen.fr>
2018-02-28 11:16:50 +0100

    spec

---
+++ spec/outputs/warp10_spec.rb
@@ -0,0 +1,32 @@
+
+# encoding: utf-8
+
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/outputs/warp10'
+require 'logstash/codecs/plain'
+require 'logstash/event'
+
+describe LogStash::Outputs::Warp10 do
+  let(:output) do
+    LogStash::Outputs::Warp10.new(
+      'warp_uri' => 'http://127.0.0.1:8080/api/v0/update',
+      'token' => 'token',
+      'gts_name' => 'log',
+      'https' => 'false'
+    )
+  end
+
+  before do
+    output.register
+  end
+
+  context '#decode' do
+    it 'should show how much time takes warp10 to handle messages' do
+      counter = 0
+      while counter < 100_000
+        output.receive(LogStash::Event.new('message' => 'test' + counter.to_s, 'host' => 'test'))
+        counter += 1
+      end
+    end
+  end
+end
